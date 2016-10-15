8c31ffe67cc5b8d7b4f1b42a4d2c84b268febf77
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2016-10-15 17:59:53 -0600

    Saturday

---
+++ lib/night_write.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class NightWrite
+  attr_reader :input_file,
+              :output_file
+  attr_accessor :text
+  def initialize(input, output)
+    @input_file = input
+    @output_file = output
+  end
+
+  def output_to_terminal
+    @text = File.readlines(input_file)
+    file_chars = text.flatten.each do |line|
+      line.strip!
+    end.join("").size
+    puts "Created #{output_file} containing #{file_chars} characters"
+  end
+end
+
+running_file = ($PROGRAM_NAME == __FILE__)
+
+if running_file
+  input_file = ARGV[0]
+  output_file = ARGV[1]
+  NightWrite.new(input_file, output_file).output_to_terminal
+end
