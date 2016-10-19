b1bff149ed4e00682dc730736e57d62436b7e5b1
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2016-10-18 20:35:31 -0600

    made rakefile work

--- test/night_read_test.rb
+++ test/night_read_test.rb
@@ -6,10 +6,4 @@ class NightReadTest < Minitest::Test
   def test_night_read_class_exists
     assert_instance_of NightRead, NightRead.new
   end
-
-  def test_night_read_can_take_a_file
-    n = NightRead.new
-
-    refute_equal nil, n.
-  end
 end
